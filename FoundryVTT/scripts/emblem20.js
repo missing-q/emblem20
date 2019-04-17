b068288192c8ba64774fbeaea6ddbd668f2db528
Unknown <ivybetterit@gmail.com>
2019-04-17 14:30:12 -0400

    Items

--- FoundryVTT/scripts/emblem20.js
+++ FoundryVTT/scripts/emblem20.js
@@ -135,50 +135,175 @@ class ActorEmblem20 extends Actor {
 /* -------------------------------------------- */
 
 
-/**
- * Extend the basic ItemSheet with some very simple modifications
- */
-class SimpleItemSheet extends ItemSheet {
+ /**
+  * Override and extend the basic :class:`ItemSheet` implementation
+  */
+ class Item5eSheet extends ItemSheet {
+   constructor(item, options) {
+     super(item, options);
+     this.mce = null;
+   }
 
-  /**
-   * Extend and override the default options used by the 5e Actor Sheet
-   */
-	static get defaultOptions() {
-	  const options = super.defaultOptions;
-	  options.classes = options.classes.concat(["worldbuilding", "item-sheet"]);
-	  options.template = "../templates/item-sheet.html";
-	  options.height = 400;
-	  return options;
-  }
+   /* -------------------------------------------- */
 
-  /* -------------------------------------------- */
+   /**
+    * Use a type-specific template for each different item type
+    */
+   get template() {
+     let type = this.item.type;
+     return `public/systems/dnd5e/templates/items/item-${type}-sheet.html`;
+   }
 
-  /**
-   * Activate event listeners using the prepared sheet HTML
-   * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
-   */
-	activateListeners(html) {
-    super.activateListeners(html);
+   /* -------------------------------------------- */
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.options.editable) return;
+   /**
+    * Prepare item sheet data
+    * Start with the base item data and extending with additional properties for rendering.
+    */
+   getData() {
+     const data = super.getData();
+     data['abilities'] = game.system.template.actor.data.abilities;
+     data['damageTypes'] = CONFIG.damageTypes;
+     let types = (this.item.type === "equipment") ? "armorTypes" : this.item.type + "Types";
+     data[types] = CONFIG[types];
 
-    // Activate MCE
-	  let editor = html.find(".editor-content");
-    createEditor({
-      target: editor[0],
-      height: this.position.height - 180,
-      setup: ed => { this._mce = ed },
-      save_onsavecallback: ed => {
-        let target = editor.attr("data-edit");
-        this.item.update({[target]: ed.getContent()}, true);
-      }
-    }).then(ed => {
-      this.mce = ed[0];
-      this.mce.focus();
-    });
-  }
+     // Spell-specific data
+     if ( this.item.type === "spell" ) {
+       data["spellSchools"] = CONFIG.spellSchools;
+       data["spellLevels"] = CONFIG.spellLevels;
+     }
+
+     // Tool-specific data
+     else if ( this.item.type === "tool" ) {
+       data["proficiencies"] = CONFIG.proficiencyLevels;
+     }
+     return data;
+   }
+
+   /* -------------------------------------------- */
+
+   /**
+    * Activate listeners for interactive item sheet events
+    */
+   activateListeners(html) {
+     super.activateListeners(html);
+
+     // Activate tabs
+     new Tabs(html.find(".tabs"));
+   }
+ }
+
+ // Activate global listeners
+ Hooks.on('renderChatLog', (log, html, data) => Item5e.chatListeners(html));
+
+ // Override CONFIG
+ CONFIG.Item.sheetClass = Item5eSheet;
+
+ // Standard D&D Damage Types
+ CONFIG.damageTypes = {
+   "acid": "Acid",
+   "bludgeoning": "Bludgeoning",
+   "cold": "Cold",
+   "fire": "Fire",
+   "force": "Force",
+   "lightning": "Lightning",
+   "necrotic": "Necrotic",
+   "piercing": "Piercing",
+   "poison": "Poison",
+   "psychic": "Psychic",
+   "radiant": "Radiant",
+   "slashing": "Slashing",
+   "thunder": "Thunder",
+   "healing": "Healing"
+ };
+
+ // Weapon Types
+ CONFIG.weaponTypes = {
+   "simpleM": "Simple Melee",
+   "simpleR": "Simple Ranged",
+   "martialM": "Martial Melee",
+   "martialR": "Martial Ranged",
+   "natural": "Natural",
+   "improv": "Improvised",
+   "ammo": "Ammunition"
+ };
+
+ // Weapon Properties
+ CONFIG.weaponProperties = {
+   "thr": "Thrown",
+   "amm": "Ammunition",
+   "fir": "Firearm",
+   "rel": "Reload",
+   "two": "Two-Handed",
+   "fin": "Finesse",
+   "lgt": "Light",
+   "ver": "Versatile",
+   "hvy": "Heavy",
+   "rch": "Reach"
+ };
+
+ // Equipment Types
+ CONFIG.armorTypes = {
+   "clothing": "Clothing",
+   "light": "Light Armor",
+   "medium": "Medium Armor",
+   "heavy": "Heavy Armor",
+   "bonus": "Magical Bonus",
+   "natural": "Natural Armor",
+   "shield": "Shield"
+ };
+
+ // Consumable Types
+ CONFIG.consumableTypes = {
+   "potion": "Potion",
+   "scroll": "Scroll",
+   "wand": "Wand",
+   "rod": "Rod",
+   "trinket": "Trinket"
+ };
+
+ // Spell Types
+ CONFIG.spellTypes = {
+   "attack": "Spell Attack",
+   "save": "Saving Throw",
+   "heal": "Healing",
+   "utility": "Utility"
+ };
+
+ // Spell Schools
+ CONFIG.spellSchools = {
+   "abj": "Abjuration",
+   "con": "Conjuration",
+   "div": "Divination",
+   "enc": "Enchantment",
+   "evo": "Evocation",
+   "ill": "Illusion",
+   "nec": "Necromancy",
+   "trs": "Transmutation",
+ };
+
+ // Spell Levels
+ CONFIG.spellLevels = {
+   0: "Cantrip",
+   1: "1st Level",
+   2: "2nd Level",
+   3: "3rd Level",
+   4: "4th Level",
+   5: "5th Level",
+   6: "6th Level",
+   7: "7th Level",
+   8: "8th Level",
+   9: "9th Level"
+ };
+
+ // Feat Types
+ CONFIG.featTypes = {
+   "passive": "Passive Ability",
+   "attack": "Ability Attack",
+   "ability": "Generic Action"
+ };
 }
+
 
 CONFIG.Item.sheetClass = SimpleItemSheet;
 
